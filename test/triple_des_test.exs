c7f60ea3c066a82ea06e64f81fa39b1125e30984
Zack Siri <zack@artellectual.com>
2022-05-17 11:59:27 +0700

    update test case for tiple des

--- test/triple_des_test.exs
+++ test/triple_des_test.exs
@@ -3,10 +3,17 @@ defmodule TripleDesTest do
   doctest TripleDes
 
   @key "A1993AB2022AC999992222AF"
+  @key2 "B2003AB2022AC999992222AF"
 
-  test "can encrypt and decrypt" do
+  test "can encrypt and decrypt without iv" do
     encrypted = TripleDes.encrypt("somethingwickedthiswaycomes.....", @key, :des3_ecb)
 
     assert "somethingwickedthiswaycomes....." == TripleDes.decrypt(encrypted, @key, :des3_ecb)
+  end
+
+  test "can encrypt but fail decription due to wrong key" do
+    encrypted = TripleDes.encrypt("somethingwickedthiswaycomes.....", @key, :des3_ecb)
+
+    assert TripleDes.decrypt(encrypted, @key2, :des3_ecb) != "somethingwickedthiswaycomes....."
   end
 end
